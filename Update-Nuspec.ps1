a518c49fc583754cb067b3557b254d09ae85e934
Jacek Kolonko <jkolonko@objectivity.co.uk>
2017-12-22 15:16:18 +0100

    Make package use csproj instead of nuspec

--- Update-Nuspec.ps1
+++
@@ -1,7 +0,0 @@
-﻿param([string]$version=1.0.0)
-$sfbDir = (get-item $PSScriptRoot )
-$appConfigFile = [IO.Path]::Combine($sfbDir, 'Objectivity.Bot.Tests.Stories\Objectivity.Bot.Tests.Stories.nuspec')
-$appConfig = New-Object XML;
-$appConfig.Load($appConfigFile);
-$appConfig.package.metadata.version = "$version";
-$appConfig.Save($appConfigFile);
